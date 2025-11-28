c747a5ce2b06f224e8c20d6340da9871b2ac5866
Alfonso <alfcanfild@gmail.com>
2025-11-28 08:20:26 -0300

    Foi alterado o @media de todas as páginas, a responsividade está funcionando agora. Falta arrumar o botão de calcular que parou de funcionar.

--- script.js
+++ script.js
@@ -164,14 +164,3 @@ btnSalvarRegistro.onclick = () => {
 
   atualizarTabelaEChart();
 };
-
-
-// ===== BOTÃO HAMBURGUER PARA MOBILE =====
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
-});
-
